18bda116d408a766d135d613452c9084371ec276
José Quintana <76418657+SlowpyWalnout@users.noreply.github.com>
2026-09-15 18:44:07 -0600

    feat: sanitization and dependencies migration

--- frontend/lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
+++ frontend/lib/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart
@@ -4,10 +4,10 @@ import '../../../../domain/entities/article.dart';
 
 abstract class RemoteArticlesState extends Equatable {
   final List<ArticleEntity> ? articles;
-  final DioError ? error;
-  
+  final DioException ? error;
+
   const RemoteArticlesState({this.articles,this.error});
-  
+
   @override
   List<Object> get props => [articles!, error!];
 }
@@ -21,5 +21,5 @@ class RemoteArticlesDone extends RemoteArticlesState {
 }
 
 class RemoteArticlesError extends RemoteArticlesState {
-  const RemoteArticlesError(DioError error) : super(error: error);
+  const RemoteArticlesError(DioException error) : super(error: error);
 }
